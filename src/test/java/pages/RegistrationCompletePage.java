c1f20e6b08524564e953256d8435deae05c828b4
vamsipk145 <vamsipk145@gmail.com>
2024-08-06 12:20:55 +0530

    changed eeee

--- src/test/java/pages/RegistrationCompletePage.java
+++ src/test/java/pages/RegistrationCompletePage.java
@@ -30,6 +30,6 @@ public class RegistrationCompletePage {
     }
     public static void main(String[] args) {
         // Print a message to the console
-        System.out.println("Hello, World!");	
+        System.out.println("Hello, World!eeeee");	
     }
 }
